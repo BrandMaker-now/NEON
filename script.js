d740679bc161cb4f61aaf12dc4f6d618009b7252
BrandMaker.now <l6598668@gmail.com>
2026-02-12 16:16:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ function updateStatus() {
     }
 }
 updateStatus();
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.site-header');
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
