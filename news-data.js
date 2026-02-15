a6dfa1fe91e9f43f92421098caaabb294d2b328c
BrandMaker.now <l6598668@gmail.com>
2026-02-15 10:21:53 +0900

    Update news-data.js

--- news-data.js
+++ news-data.js
@@ -91,14 +91,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
-
-
-const btn = document.getElementById('js-scroll-top');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
-    btn.classList.add('is-show');
-  } else {
-    btn.classList.remove('is-show');
-  }
-});
